2fb7c12fce0cfa4d44a2fbb3882f39b5ded734b7
Katja Potensky <katja.potensky@adesso.at>
2022-10-07 17:21:28 +0200

    feat(task-05): observing state

--- src/state.tsx
+++ src/state.tsx
@@ -4,6 +4,7 @@ import { match } from "ts-pattern";
 import { Row } from "./data";
 import { flow, pipe } from "fp-ts/lib/function";
 import { option } from "fp-ts";
+import { BehaviorSubject, distinctUntilChanged, map, Observable } from "rxjs";
 
 type State = {
   data: Row[];
@@ -36,8 +37,10 @@ const reducer: React.Reducer<State, Action> = (state, action) =>
     }))
     .exhaustive();
 
-const context = React.createContext<[State, React.Dispatch<Action>]>([
-  initialState,
+const context = React.createContext<
+  [BehaviorSubject<State>, React.Dispatch<Action>]
+>([
+  new BehaviorSubject(initialState),
   () => {
     /* ignore */
   },
@@ -46,11 +49,35 @@ const context = React.createContext<[State, React.Dispatch<Action>]>([
 export const StateProvider = function StateProvider({
   children,
 }: React.PropsWithChildren) {
-  const value = React.useReducer(reducer, initialState);
-  return <context.Provider value={value}>{children}</context.Provider>;
+  const store = React.useMemo(() => new BehaviorSubject(initialState), []);
+  const dispatch = React.useCallback(
+    (action: Action) => {
+      const next = reducer(store.value, action);
+      store.next(next);
+    },
+    [store]
+  );
+  return (
+    <context.Provider value={[store, dispatch]}>{children}</context.Provider>
+  );
 };
 
 const useState = () => React.useContext(context)[0];
+
+function useSelector<T>(selector: (state: State) => T) {
+  const state = useState();
+  const [selected, setSelected] = React.useState(() => selector(state.value));
+
+  React.useEffect(() => {
+    const subscription = state
+      .pipe(map(selector), distinctUntilChanged())
+      .subscribe((selected) => setSelected(selected));
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return selected;
+}
+
 const useDispatch = () => React.useContext(context)[1];
 
 const selectDatasetRows = ({ dataset, data }: State): Row[] =>
@@ -68,7 +95,7 @@ const selectFilteredRows = (state: State) => {
   );
 };
 
-export const useFilteredRows = flow(useState, selectFilteredRows);
+export const useFilteredRows = () => useSelector(selectFilteredRows);
 
 export const useSetData = () => {
   const dispatch = useDispatch();
